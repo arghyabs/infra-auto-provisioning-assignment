c9152614a97d88c4c1c90234b30a8cb24d0b124b
Arghya Basu <arghyabs@gmail.com>
2019-09-27 15:49:04 +0000

    Test

--- create.sh
+++ create.sh
@@ -1,1 +1,5 @@
+# !/bin/bash
+
+set -e
+
 gcloud deployment-manager deployments create deployment-with-2-vms --config two-vms.yaml
--- delete.sh
+++ delete.sh
@@ -1,1 +1,5 @@
+# !/bin/bash
+
+set -e
+
 gcloud deployment-manager deployments delete deployment-with-2-vms
